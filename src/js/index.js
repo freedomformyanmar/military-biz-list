5737c90edc87b1b14b7183eb98a1aea8c5240830
randomguy <random@random.com>
2021-02-05 14:57:42 +0630

    now it is eazy to extend and work properly

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
+function groupBy(ac, dataLists) {
+  return {
+    [dataLists[0].title]: [...dataLists.slice(1, dataLists.length)],
+    ...ac,
+  };
+}
+function searchRule(searchString, value) {
+  var regex = new RegExp(searchString, 'gi');
+  return regex.test(value.split(' ').join(''));
+}
 new Vue({
   el: '#app',
   data: {
@@ -7,32 +17,26 @@ new Vue({
   },
   async created() {
     const businessesData = await loadJsonFiles();
-    this.businesses = businessesData.reduce((ac, dataLists) => {
-      return {
-        [dataLists[0].title]: [...dataLists.slice(1, dataLists.length)],
-        ...ac,
-      };
-    }, {});
+    this.businesses = businessesData.reduce(groupBy, {});
     this.resources = this.businesses;
   },
   watch: {
     searchString: function () {
-      var obj = {};
-      Object.entries(this.resources).map(([key, arr]) => {
-        return arr.filter((el) => {
-          var regex = new RegExp(this.searchString, 'gi');
-          return regex.test(el.product) || regex.test(key);
-        });
-      });
-      filter((el) => el.length > 0).forEach((el) => {
-        if (obj[el[0].industry]) {
-          obj[el[0].industry].push(...el);
-        } else {
-          obj[el[0].industry] = [];
-          obj[el[0].industry].push(...el);
-        }
-      });
-      this.businesses = obj;
+      this.businesses = Object.entries(this.resources)
+        .map(([key, arr]) => {
+          const searchFilter = arr.filter((el) => {
+            return (
+              searchRule(this.searchString, el.product) ||
+              searchRule(this.searchString, key)
+            );
+          });
+          return (
+            searchFilter.length > 0 && {
+              [key]: searchFilter,
+            }
+          );
+        })
+        .reduce((ac, data) => ({ ...ac, ...data }), {});
     },
   },
 });
