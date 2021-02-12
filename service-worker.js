80e8be502597ad7c7675414889d01b4925aa427d
aungKhantPaing <25960342+aungKhantPaing@users.noreply.github.com>
2021-02-12 22:41:52 +0630

    Revert "make pwa"
    This reverts commit 6e86fb9b0551177f4603eb65b677447784716b07.

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    new RegExp(/.*/),
-    new workbox.strategies.NetworkFirst()
-);
